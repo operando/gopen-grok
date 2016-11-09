d8c992f0060a5cdaaa32fe9b733d6511275421ca
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2016-11-10 08:47:31 +0900

    Update search android version

--- gopen-grok.go
+++ gopen-grok.go
@@ -11,7 +11,7 @@ func main() {
 	var filePath = flag.String("f", "", "File Path")
 	var definition = flag.String("d", "", "Definition")
 	var symbol = flag.String("sy", "", "Symbol")
-	var version = flag.String("v", "6.0.0_r1.0", "Version")
+	var version = flag.String("v", "7.0.0_r1.0", "Version")
 	flag.Parse()
 	sh.Command("open", "http://tools.oesf.biz/android-"+*version+"/search?q="+*fullSerch+"&defs="+*definition+"&refs="+*symbol+"&path="+*filePath+"&hist=").Run()
 }
