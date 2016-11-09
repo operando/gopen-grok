ec42c4887e3668abf258bed27e8078fca470e34a
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2016-11-10 08:59:59 +0900

    Change the version for search

--- gopen-grok.go
+++ gopen-grok.go
@@ -6,6 +6,15 @@ import (
 	"github.com/codeskyblue/go-sh"
 )
 
+func conversionSearchVersion(version string) string {
+	switch version {
+	case "6":
+	case "6.0":
+		return "6.0.0_r1.0"
+	}
+	return "7.0.0_r1.0"
+}
+
 func main() {
 	var fullSerch = flag.String("s", "", "Full Search")
 	var filePath = flag.String("f", "", "File Path")
@@ -13,5 +22,5 @@ func main() {
 	var symbol = flag.String("sy", "", "Symbol")
 	var version = flag.String("v", "7.0.0_r1.0", "Version")
 	flag.Parse()
-	sh.Command("open", "http://tools.oesf.biz/android-"+*version+"/search?q="+*fullSerch+"&defs="+*definition+"&refs="+*symbol+"&path="+*filePath+"&hist=").Run()
+	sh.Command("open", "http://tools.oesf.biz/android-"+conversionSearchVersion(*version)+"/search?q="+*fullSerch+"&defs="+*definition+"&refs="+*symbol+"&path="+*filePath+"&hist=").Run()
 }
