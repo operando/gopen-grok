a0328028694cb4ca0905354320e29a2c166f5ca7
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2016-11-13 12:26:36 +0900

    Fix typo

--- gopen-grok.go
+++ gopen-grok.go
@@ -16,11 +16,11 @@ func conversionSearchVersion(version string) string {
 }
 
 func main() {
-	var fullSerch = flag.String("s", "", "Full Search")
+	var fullSearch = flag.String("s", "", "Full Search")
 	var filePath = flag.String("f", "", "File Path")
 	var definition = flag.String("d", "", "Definition")
 	var symbol = flag.String("sy", "", "Symbol")
 	var version = flag.String("v", "7.0.0_r1.0", "Version")
 	flag.Parse()
-	sh.Command("open", "http://tools.oesf.biz/android-"+conversionSearchVersion(*version)+"/search?q="+*fullSerch+"&defs="+*definition+"&refs="+*symbol+"&path="+*filePath+"&hist=").Run()
+	sh.Command("open", "http://tools.oesf.biz/android-"+conversionSearchVersion(*version)+"/search?q="+*fullSearch +"&defs="+*definition+"&refs="+*symbol+"&path="+*filePath+"&hist=").Run()
 }
